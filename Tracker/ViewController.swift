f31b3dbd4d73258601b79234ca2cee0551d1ac57
Mike Somov <mikhail.somov@laniakea-continuum.com>
2025-03-25 00:39:28 +0300

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Mike Somov on 25.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
